9ead81f33693e15c866435c869370bf5f2ed5954
Daren Card <dcard@uta.edu>
2018-03-07 20:44:26 -0600

    have password reported to user

--- rstudio_setup.bash
+++ rstudio_setup.bash
@@ -20,12 +20,12 @@ fi
 
 # prompt user to enter new password
 # this seems to always be necessary, so just have students do it by default
-echo -e "Please choose a password for logging into RStudio."
 echo "${user}:${pass}" | sudo chpasswd && \
 echo -e "\nYou will need this password to log into RStudio."
 
 # tell students the username they need to login
 echo -e "\nYour RStudio username is ${user}."
+echo -e "Your RStudio password is ${pass}."
 
 # tell students the URL
 echo -e "\nYour RStudio Web server is running at: http://$(hostname):8787/"
